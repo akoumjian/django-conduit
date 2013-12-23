ae119f3c1c74c0f43ba35baf63d23129c68e1a7d
Alec Koumjian <akoumjian@gmail.com>
2013-12-22 18:32:40 -0800

    resolves #11 and resolves #9

--- conduit/tests/test_custom_pkfield_overrides.py
+++ conduit/tests/test_custom_pkfield_overrides.py
@@ -1,15 +1,10 @@
-import datetime
-import dateutil
 from django.conf.urls import patterns, url, include
 from conduit.api import ModelResource, Api
-from conduit.api.fields import ForeignKeyField, ManyToManyField
-from conduit.exceptions import HttpInterrupt
 import example
 import example.urls
-from example.models import Bar, Foo, Baz
+from example.models import Bar
 from conduit.tests.testcases import ConduitTestCase
 import uuid
-import sys
 
 
 class UriTestCase(ConduitTestCase):
@@ -23,13 +18,14 @@ class UriTestCase(ConduitTestCase):
             class Meta(ModelResource.Meta):
                 model = Bar
                 pk_field = 'name'
+                resource_name = 'custom_pk'
         self.resource = TestResource()
         self.resource.Meta.api = Api(name="v1")
         self.resource.Meta.api.register(TestResource())
 
         # override urls
         self.original_urls = example.urls.urlpatterns
-        example.urls.urlpatterns = patterns(
+        example.urls.urlpatterns += patterns(
             '',
             url(r'^api/', include(self.resource.Meta.api.urls))
         )
@@ -42,6 +38,5 @@ class UriTestCase(ConduitTestCase):
         bar = Bar( name=str( uuid.uuid4() )[:8] )  
         bar.save()
         resource_uri = self.resource._get_resource_uri( obj=bar )
-        print >> sys.stdout, resource_uri
-        self.assertEqual(resource_uri, '/api/{0}/bar/{1}/'.format(self.resource.Meta.api.name, bar.name))
+        self.assertEqual(resource_uri, '/api/{0}/custom_pk/{1}/'.format(self.resource.Meta.api.name, bar.name))
 
