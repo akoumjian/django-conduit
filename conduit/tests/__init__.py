878fc4b3dc6dc0ed9f1319f97f107337b3a1edeb
thebigspoon <gregcorradini@gmail.com>
2014-04-23 17:05:33 -0700

    git create tests for ModelResources where 1) our methods are module-level functions 2) our methods are from mixin 3) we are passing conduit subclasses as self to another class method (this resolves merge conflicts )

--- conduit/tests/__init__.py
+++ conduit/tests/__init__.py
@@ -3,3 +3,4 @@
 ## https://docs.djangoproject.com/en/dev/releases/1.6/#backwards-incompatible-changes-in-1-6
 from conduit.test.test_modelresource_methods import *
 from conduit.test.test_custom_pkfield_overrides import *
+from conduit.test.test_modelresource_conduit_formats import *
