f0782b9c5285e78ddb6d08a2a1b51c07b20a1e4b
Alec Koumjian <akoumjian@gmail.com>
2014-06-19 15:36:36 +1000

    set new version

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ except ImportError:
 
 setup(
     name='django-conduit',
-    version='0.1.0',
+    version='0.1.1',
     description='Easy and powerful REST APIs for Django.',
     author='Alec Koumjian',
     author_email='akoumjian@gmail.com',
