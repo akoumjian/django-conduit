5e51c1626d6ae4cfd4013b8596a49ec524cbf349
Greg Corradini <gregcorradini@gmail.com>
2014-11-13 13:26:58 +0000

    try it with create extension

--- travis_postgis_setup.sh
+++ travis_postgis_setup.sh
@@ -8,19 +8,11 @@ set -ev
 #    psql -U postgres -d geoexample -c "create extension postgis"
 #fi
 
-# print environment variables
-env
-
-POSTGIS_SQL_PATH=`pg_config --sharedir`/contrib/postgis-2.0
-# Creating the template spatial database.
 createdb -U postgres -E UTF8 template_postgis
-#createlang -U postgres -d template_postgis plpgsql # Adding PLPGSQL language support.
-# Allows non-superusers the ability to create from this template
+# allow non-superusers the ability to create from this template
 psql -U postgres -d postgres -c "UPDATE pg_database SET datistemplate='true' WHERE datname='template_postgis';"
-# Loading the PostGIS SQL routines
-psql -U postgres -d template_postgis -f $POSTGIS_SQL_PATH/postgis.sql 
-psql -U postgres -d template_postgis -f $POSTGIS_SQL_PATH/spatial_ref_sys.sql
-# Enabling users to alter spatial tables.
+psql -U postgres -d template_postgis -c "create extension postgis;"
+# enable users to alter spatial tables.
 psql -U postgres -d template_postgis -c "GRANT ALL ON geometry_columns TO PUBLIC;"
 psql -U postgres -d template_postgis -c "GRANT ALL ON geography_columns TO PUBLIC;"
 psql -U postgres -d template_postgis -c "GRANT ALL ON spatial_ref_sys TO PUBLIC;"
