5e8cc4eed56625f9edce580a6cb92cf663a0211b
Greg Corradini <gregcorradini@gmail.com>
2014-11-13 03:59:39 +0000

    update geodefault database settingss for Travis CI

--- example/example/settings.py
+++ example/example/settings.py
@@ -29,10 +29,10 @@ DATABASES = {
         'ENGINE': 'django.contrib.gis.db.backends.postgis' , #'django.db.backends.sqlite3' Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'geoexample',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
-        'USER': 'gcorradini',
-        'PASSWORD': 'gcorradini',
-        'HOST': 'localhost',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
-        'PORT': '5432',                      # Set to empty string for default.
+        'USER': 'postgres',
+        'PASSWORD': '', # Travis CI does not have a password for this
+        'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+        'PORT': '',                      # Set to empty string for default.
     }
 }
 
