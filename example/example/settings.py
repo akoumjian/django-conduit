f5779866051f044c33a3f5e4d84c78ef066cf59f
Greg Corradini <gregcorradini@gmail.com>
2014-11-13 04:27:42 +0000

    try setting POSTGIS_VERSION explicitly

--- example/example/settings.py
+++ example/example/settings.py
@@ -38,6 +38,8 @@ DATABASES = {
 
 DATABASE_ROUTERS = ['example.geo_db_router.GeoDbRouter']
 
+POSTGIS_VERSION = (2, 1)
+
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
 ALLOWED_HOSTS = []
